e0dc9c65869ce79dd143e9c04a61cd9c7ba76dbe
Xuan Hung <doanxuanhung1997@gmail.com>
2020-06-05 16:50:46 +0700

    setup source

---
+++ resources/views/dashboard/roles/index.blade.php
@@ -0,0 +1,81 @@
+@extends('dashboard.base')
+
+@section('content')
+
+
+<div class="container-fluid">
+  <div class="fade-in">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card">
+          <div class="card-header"><h4>Menu roles</h4>
+          </div>
+          <div class="card-body">
+            <div class="row">
+                <a class="btn btn-lg btn-primary" href="">Add new role</a>
+            </div>
+            <br>
+            <table class="table table-striped table-bordered datatable">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Hierarchy</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            Name roles
+                        </td>
+                        <td>
+                            hierarchy
+                        </td>
+                        <td>
+                            created_at
+                        </td>
+                        <td>
+                            updated_at
+                        </td>
+                        <td>
+                            <a class="btn btn-success" href="">
+                                <i class="cil-arrow-thick-top"></i> 
+                            </a>
+                        </td>
+                        <td>
+                            <a class="btn btn-success" href="">
+                                <i class="cil-arrow-thick-bottom"></i>  
+                            </a>
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-primary">Show</a>
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-primary">Edit</a>
+                        </td>
+                        <td>
+                            <form action="" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+@section('javascript')
+
+@endsection
---
+++ resources/views/dashboard/shared/nav-builder.blade.php
@@ -0,0 +1,35 @@
+
+<div class="c-sidebar-brand">
+    <img class="c-sidebar-brand-full" src="/assets/brand/coreui-base-white.svg" width="118" height="46" alt="CoreUI Logo">
+    <img class="c-sidebar-brand-minimized" src="assets/brand/coreui-signet-white.svg" width="118" height="46" alt="CoreUI Logo">
+</div>
+<ul class="c-sidebar-nav">
+    <li class="c-sidebar-nav-item">
+        <a class="c-sidebar-nav-link" href="">
+
+            <i class="c-sidebar-nav-icon"></i>
+            Dashboarh
+        </a>
+    </li>
+    <li class="c-sidebar-nav-dropdown"><a class="c-sidebar-nav-dropdown-toggle" href="#">
+        <svg class="c-sidebar-nav-icon">
+            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-code"></use>
+        </svg> Editors</a>
+        <ul class="c-sidebar-nav-dropdown-items">
+            <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="editors/code-editor.html">
+                <svg class="c-sidebar-nav-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-notes"></use>
+                </svg> Code Editor<span class="badge badge-danger">PRO</span></a></li>
+                <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="editors/markdown-editor.html">
+                    <svg class="c-sidebar-nav-icon">
+                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-code"></use>
+                    </svg> Markdown<span class="badge badge-danger">PRO</span></a></li>
+                    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="editors/text-editor.html">
+                        <svg class="c-sidebar-nav-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-notes"></use>
+                        </svg> Rich Text Editor<span class="badge badge-danger">PRO</span></a></li>
+                    </ul>
+                </li>
+            </ul>
+            <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
+        </div>
